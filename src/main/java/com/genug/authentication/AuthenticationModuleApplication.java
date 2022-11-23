a94499a4d03b1a7420d7b9af3329046f321f2eb6
genugx-x <qparanoya@gmail.com>
2022-11-23 15:01:21 +0900

    프로젝트 초기화

---
+++ src/main/java/com/genug/authentication/AuthenticationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.genug.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationModuleApplication.class, args);
+    }
+
+}
